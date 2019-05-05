9c7ce84068a80abe230bcb14a9ad4e2db1d6b425
zhangwenhao <zhangwenhao@iboxpay.com>
2019-05-05 09:30:20 +0800

    creat new model ;1:test methoed user public 2:submoudle extends root moudle dependce

---
+++ spring-transaction/src/main/java/com.conan.springtransaction/SpringTransaction1.java
@@ -0,0 +1,10 @@
+package com.conan.springtransaction;
+
+public class SpringTransaction1 {
+
+    public void too() {
+        System.out.println("xxx");
+    }
+
+
+}
---
+++ spring-transaction/src/test/java/com.conan.springtransaction/Test_Spring_transaction1.java
@@ -0,0 +1,26 @@
+package com.conan.springtransaction;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class Test_Spring_transaction1 {
+
+    @Before
+    public void before() {
+        System.out.println("before");
+    }
+
+    @After
+    public void after() {
+        System.out.println("after");
+    }
+
+    @Test
+    public void test_model() {
+        System.out.println("test_model");
+    }
+}
