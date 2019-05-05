e043147935e3515d3a783df12180f347df04a694
zhangwenhao <zhangwenhao@iboxpay.com>
2019-05-05 16:56:25 +0800

    声明式事务管理方式2 最原始状态

---
+++ spring-transaction/src/test/java/com.conan.springtransaction/TestTransactionManagerStatement2.java
@@ -0,0 +1,27 @@
+package com.conan.springtransaction;
+
+import com.conan.springtransaction.service.AccountService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * spring 声明式事务管理 方式1
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:springconfig_statement2.xml")
+public class TestTransactionManagerStatement2 {
+
+    @Resource(name = "accountService")
+    private AccountService accountService;
+
+    @Test
+    public void testTransactionManagerStatement2() {
+
+        accountService.transfer("aaa", "bbb", 200d);
+
+    }
+}
